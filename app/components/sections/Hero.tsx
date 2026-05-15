1dcfa24a12797fb48dda3b5353aecc3ad43d5e02
eneekoruiz <eruiz084@ikasle.ehu.eus>
2026-05-15 08:46:35 +0200

    fix: import useTheme in Hero component to fix build error

--- app/components/sections/Hero.tsx
+++ app/components/sections/Hero.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useRef, useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
 import gsap from 'gsap';
 import { useDeviceTilt } from '../../hooks/useDeviceTilt';
 
